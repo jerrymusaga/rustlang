80c2cde7677e717f3de60279d8c78b679137f0ea
jerrymusaga <musagatechnology@gmail.com>
2022-07-29 12:56:50 +0100

    init commit

---
+++ part1/one/target/rls/debug/deps/one-528c965c20927658.d
@@ -0,0 +1,5 @@
+/home/musaga/RustIntensePractice/part1/one/target/rls/debug/deps/one-528c965c20927658.rmeta: src/main.rs
+
+/home/musaga/RustIntensePractice/part1/one/target/rls/debug/deps/one-528c965c20927658.d: src/main.rs
+
+src/main.rs:
